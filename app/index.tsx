d90e93a455d283377f87fead463f087820c462eb
Shivu031 <128800786+Shivu031@users.noreply.github.com>
2024-12-23 13:35:40 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View className='flex items-center justify-center flex-1'>
+      <Text>This is react native app</Text>
+    </View>
+  )
+}
+
+export default Index
